d654c34a817a82ca42e9e06070b6390bfc4eaabe
Rei <reisegev@yahoo.com>
2021-02-03 18:16:39 +0200

    Update GameManager.cs

--- WildNetter/Assets/Scripts/GameManager.cs
+++ WildNetter/Assets/Scripts/GameManager.cs
@@ -16,20 +16,20 @@ public class GameManager : MonoSingleton<GameManager>
             PlayerManager._Instance,
             PlayerStats._Instance,
             TotemManager._Instance,
-        UiManager._Instance,
-        InputManager._Instance,
-        PlayerCombat._Instance,
-        PlayerMovement._Instance,
-        SoundManager._Instance,
-        EnemyManager._Instance,
-        EnemySpawner._Instance
+            UiManager._Instance,
+            InputManager._Instance,
+            PlayerCombat._Instance,
+            PlayerMovement._Instance,
+            SoundManager._Instance,
+            EnemyManager._Instance,
+            EnemySpawner._Instance
         };
 
         for (int i = 0; i < singletons.Length; i++)
         {
             if (singletons[i] != null)
                 singletons[i].Init();
-            
+
 
         }
     }
