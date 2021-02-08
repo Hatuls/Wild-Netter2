f25b5ef7759e3504c6d06256edc582e922aa7574
Aquid16 <54771966+Aquid16@users.noreply.github.com>
2021-02-09 01:56:39 +0200

    a

--- WildNetter/Assets/Scripts/UI/GameUI.cs
+++ WildNetter/Assets/Scripts/UI/GameUI.cs
@@ -6,6 +6,14 @@ public class GameUI : MonoBehaviour
 {
     private bool guiOn = true;
     private bool playerMenuOn = false;
+    private bool inGamePopUpOn = false;
+    private bool inventoryMenuOn = false;
+    private bool pauseMenuOn = false;
+    private bool pausePopUpOn = false;
+    private bool inGameSettingsOn = false;
+    private bool inGameSoundSettings = false;
+    private bool inGameGraphicsSettings = false;
+    private bool inGameControlsSettings = false;
     private void Start()
     {
         UiManager._Instance.ToggleGUIinScene(guiOn);
@@ -14,6 +22,87 @@ public class GameUI : MonoBehaviour
     public void PlayerMenu()
     {
         playerMenuOn = true;
+        inGamePopUpOn = true;
+        inventoryMenuOn = false;
         UiManager._Instance.TogglePlayerMenu(playerMenuOn);
+        UiManager._Instance.ToggleInGamePopUp(inGamePopUpOn);
+        UiManager._Instance.ToggleInventoryMenu(inventoryMenuOn);
+    }
+
+    public void InventoryMenu()
+    {
+        playerMenuOn = false;
+        inGamePopUpOn = true;
+        inventoryMenuOn = true;
+        UiManager._Instance.ToggleInGamePopUp(inGamePopUpOn);
+        UiManager._Instance.ToggleInventoryMenu(inventoryMenuOn);
+        UiManager._Instance.TogglePlayerMenu(playerMenuOn);
+    }
+
+    public void PauseMenu()
+    {
+        pausePopUpOn = true;
+        pauseMenuOn = true;
+        inGameSettingsOn = false;
+        UiManager._Instance.TogglePauseMenuPopUp(pausePopUpOn);
+        UiManager._Instance.TogglePauseMenu(pauseMenuOn);
+        UiManager._Instance.ToggleInGameOptionsMenu(inGameSettingsOn);
+    }
+    public void PauseMenuSettings()
+    {
+        pausePopUpOn = true;
+        pauseMenuOn = false;
+        inGameSettingsOn = true;
+        inGameSoundSettings = true;
+        UiManager._Instance.ToggleInGameOptionsMenu(inGameSettingsOn);
+        UiManager._Instance.TogglePauseMenuPopUp(pausePopUpOn);
+        UiManager._Instance.TogglePauseMenu(pauseMenuOn);
+        UiManager._Instance.ToggleSoundSettings(inGameSoundSettings);
+    }
+
+    public void SoundSettings()
+    {
+        inGameControlsSettings = false;
+        inGameGraphicsSettings = false;
+        inGameSoundSettings = true;
+        UiManager._Instance.ToggleControlsSettings(inGameControlsSettings);
+        UiManager._Instance.ToggleGraphicsSettings(inGameGraphicsSettings);
+        UiManager._Instance.ToggleSoundSettings(inGameSoundSettings);
+    }
+
+    public void Graphicsettings()
+    {
+        inGameControlsSettings = false;
+        inGameGraphicsSettings = true;
+        inGameSoundSettings = false;
+        UiManager._Instance.ToggleControlsSettings(inGameControlsSettings);
+        UiManager._Instance.ToggleGraphicsSettings(inGameGraphicsSettings);
+        UiManager._Instance.ToggleSoundSettings(inGameSoundSettings);
+    }
+
+    public void ControlsSettings()
+    {
+        inGameControlsSettings = true;
+        inGameGraphicsSettings = false;
+        inGameSoundSettings = false;
+        UiManager._Instance.ToggleControlsSettings(inGameControlsSettings);
+        UiManager._Instance.ToggleGraphicsSettings(inGameGraphicsSettings);
+        UiManager._Instance.ToggleSoundSettings(inGameSoundSettings);
+    }
+
+    public void BackFromSettings()
+    {
+        PauseMenu();
+    }
+
+    public void Continue()
+    {
+        pausePopUpOn = false;
+        UiManager._Instance.TogglePauseMenuPopUp(pausePopUpOn);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
--- WildNetter/Assets/Scripts/UI/UiManager.cs
+++ WildNetter/Assets/Scripts/UI/UiManager.cs
@@ -19,6 +19,13 @@ public class UiManager : MonoSingleton<UiManager>
     [SerializeField] GameObject optionsMenu;
     [SerializeField] GameObject inventorySlotHolder;
     [SerializeField] GameObject playerMenu;
+    [SerializeField] GameObject inGamePopUp;
+    [SerializeField] GameObject pausePopUp;
+    [SerializeField] GameObject pauseScreen;
+    [SerializeField] GameObject inGameOptionsMenu;
+    [SerializeField] GameObject inGameSoundOptions;
+    [SerializeField] GameObject inGameGraphicsOptions;
+    [SerializeField] GameObject inGameControlsOptions;
     [SerializeField] GameObject gui;
     [SerializeField] GameObject[] Slots;
     TextMeshProUGUI currencyTMP;
@@ -27,28 +34,28 @@ public class UiManager : MonoSingleton<UiManager>
     Item[] inventory;
     //SerializeField] TextMeshProUGUI inventoryCapacityText;
 
-     
+
     // Variables:
 
     // Getter & Setters:
 
-  
+
     public override void Init()
     {
         _wallet = PlayerWallet.GetInstance;
         _playerInventory = PlayerInventory.GetInstance;
         inventory = _playerInventory.GetInventory;
-         Slots = new GameObject[_playerInventory.maxCapacityOfItemsInList];
+        Slots = new GameObject[_playerInventory.maxCapacityOfItemsInList];
         currencyTMP = playerInventoryUIWindow.transform.Find("CurrencyText").GetComponent<TextMeshProUGUI>();
-        inventoryCapacityTMP= playerInventoryUIWindow.transform.Find("CapacityText").GetComponent<TextMeshProUGUI>();
+        inventoryCapacityTMP = playerInventoryUIWindow.transform.Find("CapacityText").GetComponent<TextMeshProUGUI>();
         for (int i = 0; i < _playerInventory.maxCapacityOfItemsInList; i++)
         {
 
-            Slots[i]= inventorySlotHolder.transform.GetChild(i).gameObject;
-   
+            Slots[i] = inventorySlotHolder.transform.GetChild(i).gameObject;
+
         }
         defaultSpriteForSlot = inventorySlotHolder.transform.GetChild(0).GetComponent<Image>().sprite;
-      UpdateInventory();
+        UpdateInventory();
     }
     public void ToggleMainMenu(bool state) 
     {
@@ -60,27 +67,27 @@ public class UiManager : MonoSingleton<UiManager>
     {
         if (!playerInventoryUIWindow.activeSelf)
             return;
-        
+
         //Need to create PlayerWallet - > _wallet
-          currencyTMP.text = string.Format("Gold : {0}    Silver : {1}    Copper : {2}", _wallet.GetSetPlayersGold, _wallet.GetSetPlayersSilver, _wallet.GetSetPlayersCopper);
-        inventoryCapacityTMP.text= string.Format("{0}/{1}", inventory.Length - _playerInventory.GetAmountOfItem(null)  , inventory.Length);
+        currencyTMP.text = string.Format("Gold : {0}    Silver : {1}    Copper : {2}", _wallet.GetSetPlayersGold, _wallet.GetSetPlayersSilver, _wallet.GetSetPlayersCopper);
+        inventoryCapacityTMP.text = string.Format("{0}/{1}", inventory.Length - _playerInventory.GetAmountOfItem(null), inventory.Length);
         string text = " / " + _playerInventory.maxCapacityOfItemsInSlot;
-    
+
         for (int i = 0; i < inventory.Length; i++)
         {
-            if ( inventory[i] == null)
-                {
-            Slots[i].GetComponentInChildren<Text>().text = "";
+            if (inventory[i] == null)
+            {
+                Slots[i].GetComponentInChildren<Text>().text = "";
                 Slots[i].GetComponent<Image>().sprite = defaultSpriteForSlot;
                 continue;
-                }
+            }
 
-         
-            
+
+
 
             if (i < inventory.Length)
             {
-                
+
 
 
                 Slots[i].GetComponent<Image>().sprite = ItemFactory._Instance.GetItemSprite(inventory[i].ID);
@@ -88,27 +95,31 @@ public class UiManager : MonoSingleton<UiManager>
                 {
 
 
-                
+
                     Slots[i].GetComponentInChildren<Text>().text = inventory[i].amount + text;
-                 
+
                 }
             }
         }
     }
-    
+
     public void ToggleInventoryMenu(bool state)
     {
 
-        if (state)
-        {
-            UpdateInventory();
-        }
+        //if (state)
+        //{
+        //    UpdateInventory();
+        //}
         playerInventoryUIWindow.SetActive(state);
     }
     public void ToggleStatsMenu(bool state) { }
     public void ToggleMapUI(bool state) { }
     public void ToggleTotemModifierMSG(bool state) { }
     public void ToggleMissionMenu(bool state) { }
+    public void ToggleInGamePopUp(bool state)
+    {
+        inGamePopUp.SetActive(state);
+    }
     public void TogglePlayerMenu (bool state)
     {
         playerMenu.SetActive(state);
@@ -128,6 +139,33 @@ public class UiManager : MonoSingleton<UiManager>
     public void ToggleNewsMenu(bool state)
     {
         newsMenu.SetActive(state);
+    }
+
+    public void TogglePauseMenu(bool state)
+    {
+        pauseScreen.SetActive(state);
+    }
+
+    public void TogglePauseMenuPopUp(bool state)
+    {
+        pausePopUp.SetActive(state);
+    }
+    public void ToggleSoundSettings(bool state)
+    {
+        inGameSoundOptions.SetActive(state);
+    }
+    public void ToggleGraphicsSettings(bool state)
+    {
+        inGameGraphicsOptions.SetActive(state);
+    }
+    public void ToggleControlsSettings(bool state)
+    {
+        inGameControlsOptions.SetActive(state);
+    }
+
+    public void ToggleInGameOptionsMenu(bool state)
+    {
+        inGameOptionsMenu.SetActive(state);
     }
     public void CloseAllMenus() { }
 
