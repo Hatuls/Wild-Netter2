4c9938db1da3cf8a5542704ee42d7e07299fc5f3
elor504 <74613768+elor504@users.noreply.github.com>
2021-04-03 15:01:55 +0300

    added leantwean

---
+++ WildNetter/Assets/WorldMapManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMapManager : MonoBehaviour
+{
+    [SerializeField] RectTransform playerToken;
+    [SerializeField] RectTransform[] mapLocations;
+    [SerializeField] bool isMovingToward;
+    [SerializeField]RectTransform choosenLocation
+    {
+        set
+        {
+            if (value != null)
+            {
+                isMovingToward = true;
+            }
+            else
+            {
+                isMovingToward = false;
+            }
+            choosenLocation = value;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMovingToward)
+            GoTowardLocation();
+    }
+
+    void GoTowardLocation()
+    {
+       
+    }
+
+    public void OnClickGoToMapLocation(int index)
+    {
+        //make playertoken go to map location that he clicked on
+        choosenLocation = mapLocations[index];
+    }
+
+    void NoChoosenLocation()
+    {
+        choosenLocation = null;
+    }
+
+    void ChangeToMapScene(string SceneName)
+    {
+        Debug.Log("Go to Location: " + SceneName);
+        //when the player arrives to his destination change the scene
+    }
+}
